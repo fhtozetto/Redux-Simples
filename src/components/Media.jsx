637d21cd7414ab96b5406dd345aa1b570992a2c4
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-04-26 20:54:29 -0300

    Outros Componentes

---
+++ src/components/Media.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Média de Números" blue>
+            <div>
+                <span>
+                    <span>Resultado: </span>
+                    <strong>5</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
