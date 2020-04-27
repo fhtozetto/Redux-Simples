bee9ef587832851c97c2f39453994c1e60d4f268
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-04-27 09:53:04 -0300

    Impementacao SEM Redux

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const { min, max } = props
     return (
         <Card title="Soma de Números" green>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>10</strong>
+                    <strong>{ max + min }</strong>
                 </span>
             </div>
         </Card>
