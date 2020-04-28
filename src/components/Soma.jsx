9a9b4d60a251edc33efd1efa8e79fdd8fb3fab0b
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-04-28 07:25:08 -0300

    limpando Implementacao de Redux

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
-    const { min, max } = props
     return (
         <Card title="Soma de Números" green>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{ max + min }</strong>
+                    <strong>{20}</strong>
                 </span>
             </div>
         </Card>
